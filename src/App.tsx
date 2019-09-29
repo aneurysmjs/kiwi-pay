23c6c294745d5310280ad22d27cb7c00e70f4fc8
jero <aneurysmjs@outlook.com>
2019-09-29 22:20:09 +0300

    chore(*): initial config

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+// @flow strict
+import React, { Component } from 'react';
+
+import './assets/scss/styles.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <section>
+        <div className="mt-3">
+          <h1 className="text-center">
+            My dashboard
+          </h1>
+        </div>
+      </section>
+    );
+  }
+};
+
+export default App;
