b3ecc97511af8814a0536e893f1d86a40339a224
jero <aneurysmjs@outlook.com>
2019-09-29 23:28:02 +0300

    fix(index.html): fix HTML markup
    close <html /> tag

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 // @flow strict
 import React, { Component } from 'react';
 
+import Header from '~/components/Header';
+
 import './assets/scss/styles.scss';
 
 class App extends Component {
   render() {
     return (
       <section>
+        <Header />
         <div className="mt-3">
           <h1 className="text-center">
             My dashboard
--- src/Main.tsx
+++ src/Main.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { render } from 'react-dom'
-import App from './App'
+import React from 'react';
+import { render } from 'react-dom';
+import App from './App';
 
 const app = document.querySelector('#app');
 
