36fd1660586e6f6f15e7b1e90e08e25f0a261055
aneurysm <aneurysmjs@outlook.com>
2021-07-03 13:02:07 +0300

    feat(*): set react-dnd

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { DndProvider } from 'react-dnd';
 import store from '~/store';
 
 import App from './App';
@@ -11,7 +11,9 @@ const app = document.querySelector('#app');
 if (app) {
   render(
     <Provider store={store}>
-      <App />
+      <DndProvider backend={HTML5Backend}>
+        <App />
+      </DndProvider>
     </Provider>,
     app,
   );
