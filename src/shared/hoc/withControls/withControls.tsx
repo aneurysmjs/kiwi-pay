9e10c8d11b020787af40d995941b96a51a7d1465
aneurysm <aneurysmjs@outlook.com>
2021-07-04 22:52:42 +0300

    feat(withcontrols): add positioning
    make control's positioning configurable via props

--- src/shared/hoc/withControls/withControls.tsx
+++ src/shared/hoc/withControls/withControls.tsx
@@ -1,4 +1,9 @@
 import { JSXElementConstructor, useRef, ReactElement, ReactNode } from 'react';
+import { faTrash } from '@fortawesome/free-solid-svg-icons/faTrash';
+import { faArrowsAlt } from '@fortawesome/free-solid-svg-icons/faArrowsAlt';
+import { faCopy } from '@fortawesome/free-solid-svg-icons/faCopy';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames';
 
 import { ConnectDragSource } from 'react-dnd';
 
@@ -6,8 +11,44 @@ import { useDragging, UseDraggingOptions } from '~/shared/hooks/useDragging/useD
 
 import './withControls.scss';
 
+type ControlsPosition = 'left' | 'top';
+
+interface ControlsProps {
+  position?: ControlsPosition;
+  dragRef: ConnectDragSource;
+}
+
+const isVertical = (position: ControlsPosition): boolean => position === 'left';
+
+const Controls = ({ dragRef, position = 'left' }: ControlsProps) => {
+  const postFix = isVertical(position) ? '-vertical' : '';
+  return (
+    <div
+      style={{ zIndex: 10 }}
+      // className="btn-group-vertical position-absolute top-50 start-0 translate-left left-n1"
+      className={classNames('position-absolute', {
+        [`btn-group${postFix}`]: true,
+        'top-50 start-0 translate-left left-n1': isVertical(position),
+        'start-0 controls-top': !isVertical(position),
+      })}
+      role="group"
+      aria-label="controls"
+    >
+      <span role="button" className="p-1 text-primary" ref={dragRef}>
+        <FontAwesomeIcon icon={faArrowsAlt} color="text-primary" />
+      </span>
+      <span role="button" className="p-1 text-primary">
+        <FontAwesomeIcon icon={faCopy} />
+      </span>
+      <span role="button" className="p-1 text-primary">
+        <FontAwesomeIcon icon={faTrash} />
+      </span>
+    </div>
+  );
+};
 export interface WithControlsProps {
   children?: ReactNode;
+  position?: ControlsPosition;
   dragOptions: Omit<UseDraggingOptions, 'containerRef'>;
 }
 interface WithControls {
@@ -15,28 +56,6 @@ interface WithControls {
     props: TProps,
   ) => ReactElement;
 }
-
-interface ControlsProps {
-  dragRef: ConnectDragSource;
-}
-
-const Controls = ({ dragRef }: ControlsProps) => (
-  <div
-    className="btn-group-vertical position-absolute top-50 start-0 translate-left left-n1"
-    role="group"
-    aria-label="controls"
-  >
-    <span role="button" className="btn btn-secondary btn-small p-1" ref={dragRef}>
-      +
-    </span>
-    <button type="button" className="btn btn-secondary btn-small p-1">
-      C
-    </button>
-    <button type="button" className="btn btn-secondary btn-small p-1">
-      X
-    </button>
-  </div>
-);
 
 // eslint-disable-next-line react/display-name
 export const withControls: WithControls = (Component) => (props) => {
@@ -50,7 +69,7 @@ export const withControls: WithControls = (Component) => (props) => {
       style={{ opacity: isDragging ? 0 : 1 }}
       data-handler-id={handlerId}
     >
-      <Controls dragRef={dragRef} />
+      <Controls position={props.position} dragRef={dragRef} />
 
       <Component {...props} />
     </div>
