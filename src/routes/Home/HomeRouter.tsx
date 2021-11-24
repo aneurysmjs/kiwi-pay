1674fa8b4b40dc26b64dc771f9501c8c9d749392
aneurysm <aneurysmjs@outlook.com>
2021-11-24 18:20:04 +0300

    fix(plop): fix dynamic route's import

--- src/routes/Home/HomeRouter.tsx
+++ src/routes/Home/HomeRouter.tsx
@@ -2,11 +2,11 @@ import { FunctionComponent } from 'react';
 import loadable from '@loadable/component';
 import { Switch, Route } from 'react-router-dom';
 
-const HomePage = loadable(() => import('~/modules/Home/pages/HomePage');
+const HomePage = loadable(() => import('~/modules/Home/pages/HomePage'));
 
 const HomeRouter: FunctionComponent = () => (
   <Switch>
-    <Route component={ HomePage } path="/" exact />
+    <Route component={HomePage} path="/" exact />
   </Switch>
 );
 
