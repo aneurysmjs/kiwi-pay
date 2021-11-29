4aa75de9d3b376793c11e653d39063b3f31da577
aneurysm <aneurysmjs@outlook.com>
2021-11-29 12:26:32 +0300

    feat(*): install and configure Jest

---
+++ jest.config.js
@@ -0,0 +1,27 @@
+const paths = require('./config/paths');
+
+module.exports = {
+  verbose: true,
+  collectCoverageFrom: ['src/**/*.{js,jsx,mjs,ts,tsx}'],
+  setupFiles: ['<rootDir>/node_modules/regenerator-runtime/runtime'],
+  // A list of paths to modules that run some code to configure
+  // or set up the testing framework before each test.
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.{js,jsx,mjs,ts,tsx}',
+    '<rootDir>/src/**/?(*.)(spec|test).{js,jsx,mjs,ts,tsx}',
+  ],
+  testEnvironment: 'jsdom',
+  testURL: 'http://localhost',
+  modulePaths: ['src'],
+  moduleNameMapper: {
+    '^~[/](.+)': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': '<rootDir>/node_modules/ts-jest',
+    '^.+\\.css$': '<rootDir>/config/jest/cssTransform.js',
+    '^(?!.*\\.(js|jsx|mjs|css|json|ts|tsx)$)': '<rootDir>/config/jest/fileTransform.js',
+  },
+  transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|ts|tsx)$'],
+  moduleDirectories: paths.resolveModules,
+  moduleFileExtensions: ['js', 'json', 'jsx', 'node', 'mjs', 'ts', 'tsx'],
+};
