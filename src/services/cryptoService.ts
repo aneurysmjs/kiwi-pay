d515f2a97f33486ebb80cfb782f7cd91bb19aeba
aneurysm <aneurysmjs@outlook.com>
2023-02-09 13:13:17 +0100

    test(cryptoservice): add test

--- src/services/cryptoService.ts
+++ src/services/cryptoService.ts
@@ -1,10 +1,10 @@
-import axios, { AxiosPromise } from 'axios';
+import axios from 'axios';
 
 import type { Crypto } from '@/store/modules/crypto/types';
 
 const CRYPTO_CURRENT_PRICE_URL = 'https://api.coindesk.com/v1/bpi/currentprice.json';
 
 // eslint-disable-next-line import/prefer-default-export
-export const getCryptoCurrentPrice = (): AxiosPromise<Crypto> => {
-  return axios.get(CRYPTO_CURRENT_PRICE_URL);
+export const getCryptoCurrentPrice = () => {
+  return axios.get<Crypto>(CRYPTO_CURRENT_PRICE_URL);
 };
