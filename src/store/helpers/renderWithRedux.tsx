7ea00fe0d3f3819f1e012eb474d9ba28d010e483
aneurysm <aneurysmjs@outlook.com>
2023-02-09 22:34:54 +0100

    fix(renderwithredux): pass middlewares
    allows proper testing for components that dispatch actions that runs through middlewares

--- src/store/helpers/renderWithRedux.tsx
+++ src/store/helpers/renderWithRedux.tsx
@@ -1,10 +1,11 @@
 import { ReactNode } from 'react';
-import { legacy_createStore as createStore, Store } from 'redux';
+import { legacy_createStore as createStore, Store, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 
 import { render, RenderResult } from '@testing-library/react';
 import { rootReducer } from '@/store/helpers/configureStore';
 import { RootState } from '@/store';
+import middlewares from '@/store/middlewares';
 
 type WithReduxConfig = {
   initialState?: RootState;
@@ -15,9 +16,14 @@ export interface RenderWithRedux extends RenderResult {
   store: Store<RootState>;
 }
 
+const middleWareEnhancer = applyMiddleware(...middlewares);
+
 export default function renderWithRedux(
   ui: ReactNode,
-  { initialState, store = createStore(rootReducer, initialState) }: WithReduxConfig = {},
+  {
+    initialState,
+    store = createStore(rootReducer, initialState, middleWareEnhancer),
+  }: WithReduxConfig = {},
 ): RenderWithRedux {
   return {
     ...render(<Provider store={store}>{ui}</Provider>),
